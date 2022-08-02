b39a01a025e19cc7a5b2f53acd4253e8ca76c25a
xgl <xglds99@163.com>
2022-08-02 16:53:05 +0800

    first commit

---
+++ vue-admin-template-master/src/api/vod/teacher.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/vod/teacher'
+
+export default {
+	//讲师列表
+    pageList(page, limit, searchObj) {
+        return request({
+            url: `${api_name}/${page}/${limit}`,
+            method: `post`,
+            data: searchObj
+        })
+    },
+    removeById(id) {
+        return request({
+          url: `${api_name}/remove/${id}`,
+          method: `delete`
+        })
+      },
+      save(teacher) {
+        return request({
+          url: `${api_name}/save`,
+          method: `post`,
+          data: teacher
+        })
+      },
+      getById(id) {
+        return request({
+            url: `${api_name}/get/${id}`,
+            method: `get`
+        })
+    },
+    batchRemove(idList) {
+        return request({
+          url: `${api_name}/batchRemove`,
+          method: `delete`,
+          data: idList
+        })
+      },
+}
